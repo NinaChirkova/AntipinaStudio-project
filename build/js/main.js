8a3f652c6fc9164368ca62220abe71fdd2781f00
Nina <chirkna@gmail.com>
2021-03-22 23:28:33 +0300

    adaptive menu

--- build/js/main.js
+++ build/js/main.js
@@ -3,17 +3,25 @@ window.onscroll = () => {
     const headerContacts = document.querySelector('.contacts');
     const svgIconInsta = document.querySelector('#icon-insta');
     const svgIconLocation = document.querySelector('#icon-location');
+    const logoSvg = document.querySelector('.logo-svg .logo-text');
     const Y = window.scrollY;
+    const navAside = document.querySelector('.nav-aside');
 
-    if(Y > 50) {
-        headerContacts.classList.add('contacts--sticky');
-        svgIconInsta.style.cssText = "fill: #000000";
-        svgIconLocation.style.cssText = "fill: #000000";
-    } else if(Y < 50) {
-        headerContacts.classList.remove('contacts--sticky');
-        svgIconInsta.style.fill = "#FFFFFF";
-        svgIconLocation.style.fill = "#FFFFFF";
+    if(!navAside.classList.contains('nav-aside--open')) {
+        if(Y > 50) {
+            headerContacts.classList.add('contacts--sticky');
+            svgIconInsta.style.cssText = "fill: #000000";
+            svgIconLocation.style.cssText = "fill: #000000";
+            logoSvg.classList.add('logo-text--dark');
+        } else if(Y < 50) {
+            headerContacts.classList.remove('contacts--sticky');
+            svgIconInsta.style.fill = "#FFFFFF";
+            svgIconLocation.style.fill = "#FFFFFF";
+            logoSvg.classList.remove('logo-text--dark');
+        }
     }
+
+    
 }
 
 // Animation
@@ -59,3 +67,45 @@ function offset(el) {
         top: rect.top + scrollTop, left: rect.left + scrollLeft
     }
 }
+
+// Navigation
+
+window.onload = function() {
+    var navToggleButton = $('.nav-toggle');
+    var navToggleSvg = $('#nav-toggle');
+	var navList = $('.nav-aside');
+	var navListOpen = 'nav-aside--open';
+	var navLink = $('.nav__link');
+	var mobile = $('.contacts');
+	var menuFixed = 'contacts--mob-fixed';
+    var logoSvg = $('.logo-svg .logo-text');
+	
+	navToggleButton.on('click', function(e) {
+		e.preventDefault();
+		navList.toggleClass(navListOpen);
+		mobile.toggleClass(menuFixed);
+        logoSvg.toggleClass('logo-text--dark');
+
+		navButtonToggle();
+	});
+
+	navLink.on('click', function() {
+
+		if(navToggleSvg.hasClass("active"))  {
+	      navButtonToggle();
+	    } 
+	   
+		navList.removeClass(navListOpen);
+		mobile.removeClass(menuFixed);
+        logoSvg.removeClass('logo-text--dark');
+
+	});
+
+	function navButtonToggle() {
+		if(navToggleSvg.hasClass("active"))  {
+	      navToggleSvg.removeClass("active");
+	    } else {
+	      navToggleSvg.addClass("active");
+	    }
+	}
+};
